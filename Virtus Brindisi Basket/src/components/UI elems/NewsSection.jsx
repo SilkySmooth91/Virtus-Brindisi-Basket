832b019607bd4033f8c7ee3685942c068a0b47d2
SilkySmooth91 <lorenzo.olivieri13@gmail.com>
2025-08-11 13:25:44 +0200

    NewsSection animata

--- Virtus Brindisi Basket/src/components/UI elems/NewsSection.jsx
+++ Virtus Brindisi Basket/src/components/UI elems/NewsSection.jsx
@@ -1,14 +1,55 @@
+import { useEffect } from "react";
 import InfoCard from "./InfoCard"
+import MainButton from "./MainButton"
+import { motion, useAnimation } from "motion/react"
 
 export default function NewsSection() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const startAnimations = async () => {
+      await controls.start({ opacity: 1, x: 0 });
+    }
+
+    startAnimations()
+  }, [controls]);
+
   return (
-    <div className="w-full bg-black py-8 px-4 md:px-8 lg:h-screen">
-      <h2 className="text-5xl md:text-7xl font-bold uppercase text-white mb-6">
-        Ultime <span className="text-yellow-400">News</span></h2>
-      <div className="flex flex-col md:flex-row justify-center gap-6 lg:h-[calc(100vh-200px)]">
-          <InfoCard />
-          <InfoCard />
+    <div className="w-full bg-black py-8 px-4 md:px-8 md:h-screen">
+      <motion.h2 
+      className="text-5xl md:text-7xl font-bold uppercase text-white mb-6"
+      initial={{ opacity: 0, x: 400}}
+      whileInView={{ opacity: 1, x: 0}}
+      transition={{duration: 0.6}}
+      viewport={{ once: true }}>
+        Ultime <span className="text-yellow-400">News</span>
+      </motion.h2>
+
+      <div className="flex flex-col md:flex-row justify-center gap-6  md:h-96 lg:h-[calc(100vh-200px)]">
+          <motion.div
+          initial={{ opacity: 0, x: -200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}>
+            <InfoCard />
+          </motion.div>
+          <motion.div
+          initial={{ opacity: 0, x: 200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}>
+            <InfoCard />
+          </motion.div>
       </div>
+
+      <motion.div 
+      className="flex justify-center mt-4"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true }}>
+        <MainButton>Leggi altre notizie</MainButton>
+      </motion.div>
     </div>
   )
 }
